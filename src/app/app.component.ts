3b8aed44a283ccba43a62fe64de92bd4fb1b3e59
ahmedbnissa <abenaissa@welyne.com>
2024-08-17 01:33:55 +0100

    Creating Maps with angular-map-library I

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,MapComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'dataenter-app';
+}
